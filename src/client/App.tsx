3df1a130b95b14c83e6909ab8ee5b8e2f8eb3aa5
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-09-15 13:27:44 -0400

    SideBar: Links to views, Router: Components&Routes

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import SideBar from './components/SideBar';
+import Router from './components/Router';
 
 const App: React.FC = () => (
-    
+  <div style={{ display: 'flex' }}>
+    <SideBar />
+    <Router />
+  </div>
+
 );
 
-export default App
+export default App;
