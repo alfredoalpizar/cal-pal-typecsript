20b604afc7fba410e50ec496b125aa9308eb49ec
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-09-15 13:32:09 -0400

    Route components tied to paths

--- src/client/components/Router.tsx
+++ src/client/components/Router.tsx
@@ -1,11 +1,11 @@
-import * as ReactRouter from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import * as React from 'react';
 import Login from './Login';
 
 const Router: React.FC = () => (
-    <>
-        <ReactRouter.Route path="/" component={Login}/>
-    </>
+  <div style={{ flex: 1, padding: '10px' }}>
+    <Route path="/" component={Login} />
+  </div>
 );
 
-export default Router
+export default Router;
