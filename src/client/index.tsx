e6eb4a5870ebbf2fec176d1280497bb29298b958
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-09-15 13:29:48 -0400

    Wrapping App.tx with BrowserRouter

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as ReactRouter from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-    <ReactRouter.BrowserRouter>
-    <App/>
-    </ReactRouter.BrowserRouter>,
-    document.getElementById('root')
-)
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
