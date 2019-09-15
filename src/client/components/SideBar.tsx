b622fec03ad45d0f13f27ee6f0d04a9ee50f64f5
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-09-15 13:32:38 -0400

    created SideBar component with router links

--- src/client/components/SideBar.tsx
+++ src/client/components/SideBar.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
-const App: React.FC = () => (
-    
+const SideBar: React.FC = () => (
+  <div id="nav">
+    <ul style={{
+      listStyleType: 'none', padding: 0, height: '500px', width: '200px',
+    }}
+    >
+      <li><Link to="/">Search</Link></li>
+      {/* <li><Link to="/macros">Macros</Link></li>
+        <li><Link to="/entries">Entries</Link></li> */}
+    </ul>
+  </div>
 );
 
-export default App
+export default SideBar;
