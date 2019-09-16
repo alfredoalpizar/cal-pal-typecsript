9f387405f2270ea01c3e033d3ca0523556bd14e9
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-09-16 17:28:51 -0400

    added Redux Provider

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import store from './store';
+import App from './components/App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('#root'),
 );
