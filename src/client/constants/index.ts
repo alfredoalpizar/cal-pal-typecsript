c05ea6a3f168d22914065ffa0909b70b741d2c27
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-09-17 00:50:46 -0400

    implemented enum for action types

---
+++ src/client/constants/index.ts
@@ -0,0 +1,25 @@
+enum ActionTypes {
+    ADD_ENTRY,
+    DELETE_ENTRY,
+    LOAD_ENTRIES,
+    LOAD_FOODS,
+    LOAD_MACROS_PRESETS,
+    ADD_MACROS_PRESETS,
+    ADD_MACROS_TODAY,
+    LOAD_MACROS_TODAY,
+    LOGIN,
+    SIGNUP,
+}
+
+export default ActionTypes;
+// export const ADD_ENTRY = 'ADD_ENTRY';
+// export const DELETE_ENTRY = 'DELETE_ENTRY';
+// export const LOAD_ENTRIES = 'LOAD_ENTRIES';
+// export const LOAD_FOODS = 'LOAD_FOODS';
+// export const LOAD_MACROS_PRESETS = 'LOAD_MACROS_PRESETS';
+// export const ADD_MACROS_PRESETS = 'ADD_MACROS_PRESETS';
+// export const ADD_MACROS_TODAY = 'ADD_MACROS_TODAY';
+// export const LOAD_MACROS_TODAY = 'LOAD_MACROS_TODAY';
+// export const LOGIN = 'LOGIN';
+// export const SIGNUP = 'SIGNUP';
+// export const VERIFY_TOKEN = 'VERIFY_TOKEN';
