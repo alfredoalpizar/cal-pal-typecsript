14a1ebb0178e8a219c50cb9ebda3e8ef3cb7e807
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-09-17 00:52:19 -0400

    creating server.ts with its own tsconfig.json

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,9 @@
-const express = require('express');
-const path = require('path');
+import express = require('express');
+import path = require('path');
 
 const app = express();
 
-app.use('/dist', express.static('dist', {index: false}));
+app.use('/dist', express.static('dist', { index: false }));
 
 app.get('/*', (req, res) => res.sendFile(path.resolve('dist', 'index.html')));
 
