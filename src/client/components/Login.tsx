c8262b5c00d78b4542f379fd735dbf6e761ad5be
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-09-17 00:50:36 -0400

    stateful component for pw and username

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -1,7 +1,26 @@
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const Login: React.FC = () => (
-  <h1>test</h1>
-);
+const Login: React.FC = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const dispatch = useDispatch();
+  const submit = () => {
+    dispatch(login({ username, password }));
+  };
+  return (
+    <div className="login">
+
+  Username:
+      <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+  Password:
+      <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
+      <button type="button" onClick={submit}>Login</button>
+      <Link to="/signup">Signup</Link>
+
+    </div>
+  );
+};
 
 export default Login;
