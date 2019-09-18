7bc16e02a456b20293e05c77f0082a7c7586fe0f
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-09-18 10:09:38 -0400

    using express-graphql middleware with graphiql

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,16 @@
-import express = require('express');
-import path = require('path');
+import * as path from 'path';
+import * as expressGraphQL from 'express-graphql';
+import * as express from 'express';
+import schema from './schema';
+
 
 const app = express();
 
 app.use('/dist', express.static('dist', { index: false }));
-
+app.use('/graphql', expressGraphQL({
+  graphiql: true,
+  schema,
+}));
 app.get('/*', (req, res) => res.sendFile(path.resolve('dist', 'index.html')));
 
 app.listen(3000, () => console.log('listening'));
