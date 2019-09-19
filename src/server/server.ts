c2a2706af8fb868c259eb5ee5568237b014a0dd7
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-09-19 17:24:39 -0400

    implementing express-session

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as expressGraphQL from 'express-graphql';
 import * as express from 'express';
+import * as session from 'express-session';
 import schema from './schema';
 
 
@@ -11,6 +12,18 @@ app.use('/graphql', expressGraphQL({
   graphiql: true,
   schema,
 }));
+app.use(session({
+  name: 'qid',
+  secret: 'banana hammock',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+}));
+app.post('/login');
 app.get('/*', (req, res) => res.sendFile(path.resolve('dist', 'index.html')));
 
 app.listen(3000, () => console.log('listening'));
