ac49b5f77a3362770852470afc1fff5d4b48712a
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-09-22 11:26:27 -0400

    added apollo client

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import { Provider } from 'react-redux';
 import store from './store';
 import App from './containers/App';
 
+
+const client = new ApolloClient({ cache: new InMemoryCache() });
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </Provider>,
   document.querySelector('#root'),
 );
