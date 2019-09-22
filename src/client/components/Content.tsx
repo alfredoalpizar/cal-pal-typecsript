1af1b4c4fca8e65c7767ac1eafb522fae7363315
Alfredo Alpizar <alfredo.cordova.alpizar@gmail.com>
2019-09-22 11:36:07 -0400

    content component for ReactRouter content

--- src/client/components/Content.tsx
+++ src/client/components/Content.tsx
@@ -1,11 +1,21 @@
 import { Route } from 'react-router-dom';
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
+
+// import { verifyToken } from '../actions/index';
 import Login from './Login';
 
-const Content: React.FC = () => (
-  <div style={{ flex: 1, padding: '10px' }}>
-    <Route path="/" component={Login} />
-  </div>
-);
+const Content: React.FC = () => {
+  const dispatch = useDispatch();
+  // React.useEffect(() => dispatch(verifyToken()));
+  // const loggedIn = useSelector(state => state.user.loggedIn);
+  // console.log(loggedIn);
+  return (
+    <div style={{ flex: 1, padding: '10px' }}>
+      <Route path="/" component={Login} />
+      {/* <Route/> */}
+    </div>
+  );
+};
 
 export default Content;
